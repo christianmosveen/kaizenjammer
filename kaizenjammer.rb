3ac6dc645b7471477204c67910a8e8d655af186c
Christian H. Mosveen <neypalm@gmail.com>
2011-09-28 22:05:07 +0200

    Renamed table

--- kaizenjammer.rb
+++ kaizenjammer.rb
@@ -4,19 +4,19 @@ require 'datamapper'
 
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/kaizenjammer.db")
 
-class Note
+class Kaizen
 	include DataMapper::Resource
 	property :id, Serial
 	property :content, Text, :required => true
 	property :complete, Boolean, :required => true, :default => false
 	property :created_at, DateTime
-	property :updated_at, DateTime
+	property :votes, Number, :default => 0
 end
 
 DataMapper.finalize.auto_upgrade!
 
 get '/' do
-	@notes = Note.all :order => :id.desc
-	@title = 'All Notes'
+	@notes = Kaizen.all :order => :id.desc
+	@title = 'All Kaizens'
 	erb :home
 end
