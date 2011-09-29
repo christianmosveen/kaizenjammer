8bb3033e9b068e2672589fdd12508c952b228039
Christian H. Mosveen <neypalm@gmail.com>
2011-09-29 21:12:49 +0200

    Listing entries

--- kaizenjammer.rb
+++ kaizenjammer.rb
@@ -2,6 +2,20 @@ require 'rubygems'
 require 'sinatra'
 require 'datamapper'
 
+get '/' do
+	@kaizens = Kaizen.all :order => :id.desc
+	@title = 'All Kaizens'
+	erb :home
+end
+
+post '/' do
+	k = Kaizen.new
+	k.content = params[:content]
+	k.created_at = Time.now
+	k.save
+	redirect '/'
+end
+
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/kaizenjammer.db")
 
 class Kaizen
@@ -10,13 +24,7 @@ class Kaizen
 	property :content, Text, :required => true
 	property :complete, Boolean, :required => true, :default => false
 	property :created_at, DateTime
-	property :votes, Number, :default => 0
+	property :votes, Integer, :default => 0
 end
 
 DataMapper.finalize.auto_upgrade!
-
-get '/' do
-	@notes = Kaizen.all :order => :id.desc
-	@title = 'All Kaizens'
-	erb :home
-end
